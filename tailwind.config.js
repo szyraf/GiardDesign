a1278e824280b142edd673ba80e398f2c543063e
Szymon Rafałowski <szymon2489@gmail.com>
2023-08-15 17:06:18 +0200

    First commit. Tailwind works + First simple responsible section

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    // mode: 'jit',
+    content: ['./public/**/*.{html,js}'],
+    // purge: ['./src/**/*.{html,js}'],
+    theme: {
+        extend: {
+            fontFamily: {
+                inter: ['Inter', 'sans-serif'],
+                montserrat: ['Montserrat', 'sans-serif'],
+            },
+            colors: {
+                primary: '#DCC1AB',
+                secondary: '#1B5B31',
+            },
+        },
+    },
+    plugins: [],
+}
