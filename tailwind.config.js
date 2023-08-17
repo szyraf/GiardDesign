a1c071cd86cb9c35e1ea7139f3f7787b48481df7
Szymon Rafałowski <szymon2489@gmail.com>
2023-08-17 23:21:49 +0200

    started 5th section + change colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    // mode: 'jit',
-    content: ['./public/**/*.{html,js}'],
-    // purge: ['./src/**/*.{html,js}'],
-    theme: {
-        extend: {
-            fontFamily: {
-                inter: ['Inter', 'sans-serif'],
-                montserrat: ['Montserrat', 'sans-serif'],
-            },
-            colors: {
-                primary: '#DCC1AB',
-                secondary: '#1B5B31',
-                third: '#F5F0EC',
-            },
-        },
+  // mode: 'jit',
+  content: ["./public/**/*.{html,js}"],
+  // purge: ['./src/**/*.{html,js}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+        montserrat: ["Montserrat", "sans-serif"],
+      },
+      colors: {
+        primary: "#DCC1AB",
+        secondary: "#1B5B31",
+        tertiary: "#F5F0EC",
+        blackish: "#111",
+      },
     },
-    plugins: [],
-}
+  },
+  plugins: [],
+};
