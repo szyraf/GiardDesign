e8418dd00ef36d50636fb40507f303b68e212f83
Szymon Rafałowski <szymon2489@gmail.com>
2023-08-21 16:11:43 +0200

    added ligthbox - images popup

--- public/script.js
+++ public/script.js
@@ -1,4 +1,9 @@
 window.onload = () => {
+  lightbox.option({
+    resizeDuration: 200,
+    disableScrolling: true,
+  });
+
   const grid = document.querySelector(".grid-masonry");
   const masonry = new Masonry(grid, {
     itemSelector: ".grid-item-masonry",
